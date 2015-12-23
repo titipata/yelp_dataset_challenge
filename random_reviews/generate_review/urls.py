7bede85afcfda68f24f12bbb52e35e1353ed8e8b
Daniel Acuna <daniel.acuna@northwestern.edu>
2015-12-23 15:52:09 -0600

    Read reviews from django backend

--- random_reviews/generate_review/urls.py
+++ random_reviews/generate_review/urls.py
@@ -3,4 +3,5 @@ from generate_review import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^generate_review/(?P<business_type>[0-3])/(?P<stars>[1-5])/(?P<funny>[0-9]+)/(?P<cool>[0-9]+)/(?P<useful>[0-9]+)$', views.generate_review, name='generate_review'),
 )
--- random_reviews/generate_review/views.py
+++ random_reviews/generate_review/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.http import Http404 # 404
 from django.template import RequestContext
+from django.http import JsonResponse
 
 
 def index(request):
     return render_to_response('index.html')
+
+
+def generate_review(request, business_type, stars, funny, cool, useful):
+    return JsonResponse({'review': [
+                        business_type + ' ' + stars + ' ' + funny + ' ' + cool + ' ' + useful,
+                        'This business is great.',
+                        'I will come back soon']})
